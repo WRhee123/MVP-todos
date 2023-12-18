a89d6e0d11e0e944aed5fedfbba281d2124611a7
WRhee123 <rhee.william@yahoo.com>
2023-12-18 17:34:36 -0500

    able to talk to server using put route needs fix

--- src/components/EditButton2.jsx
+++ src/components/EditButton2.jsx
@@ -1,15 +1,33 @@
-const EditButton2 = ({id}) => {
+const EditButton2 = ({todo, id}) => {
   
- // const handleClick = async() => {
-    //     try {
-    //         let response = await fetch (`http://localhost:3000/api/todos/${}`)
-    //     }catch(error) {
-    //         console.log(error.stack)
-    //     }
-    // }
+ const handleEdit = async() => {
+    const inputValue = document.querySelector('#editTodo').value
+        try {
+            let response = await fetch (`http://localhost:3000/api/todos/${id}`, {
+                method: "PUT",
+                body: JSON.stringify({
+                    todo: inputValue
+                }),
+                headers: {
+                    "Content-Type": 'application/json; charset=UTF-8'
+                }
+            });
+            if(response.ok) {
+                let resData = await response.json();
+                console.log('todo was edited', resData);
+                alert(`edited todo to ${resData}`)
+            } else {
+                console.log('failed to edit todo')
+                alert('failed to edit todo')
+            }
+        }catch(error) {
+            console.log(error.stack)
+        }
+    }
+
 return (
     <div>
-        <input type='text' placeholder='edit todo here' id={id}/>
+        <input onChange={handleEdit} type='text' placeholder='edit todo here' id='editTodo'/>
     </div>
 )
 }
